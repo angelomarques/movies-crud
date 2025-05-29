9aec23dc18ca9887067b3399651f9d60128cf2e6
Angelo Marques <angeloemanuelmarques@gmail.com>
2025-05-29 15:45:16 -0300

    layout for login page

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -5,11 +5,11 @@ import { Link, Outlet } from "react-router-dom";
 export function MainLayout() {
   return (
     <div className="px-2 sm:px-5">
-      <header className="py-3 px-2 sm:px-3 flex border-b border-b-slate-200 gap-2 items-center maxw">
+      <header className="py-3 px-2 sm:px-3 flex border-b border-b-slate-200 gap-2 items-center max-w-6xl mx-auto">
         <div className="flex items-center gap-2">
           <Link to="/" className="hidden sm:block">
             <Film className="size-5" />
-            <span className="sr-only">Home page</span>
+            <span className="sr-only">Visitar Página Principal</span>
           </Link>
 
           <p className="font-semibold text-lg leading-tight">
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,104 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email("Email inválido"),
+  password: z.string().min(6, {
+    message: "A senha deve conter no mínimo 6 caracteres",
+  }),
+});
+
+type FormSchemaType = z.infer<typeof formSchema>;
+
 export function LoginPage() {
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  function onSubmit(values: FormSchemaType) {
+    console.log(values);
+  }
+
   return (
-    <div>
-      <h1 className="text-3xl">Login</h1>
+    <div className="mx-auto max-w-2xl px-2 mt-5">
+      <Card>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>
+            Digite seus dados para entrar na plataforma
+          </CardDescription>
+        </CardHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <CardContent className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Digite seu email cadastrado"
+                        {...field}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Digite sua senha"
+                        {...field}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+
+            <CardFooter>
+              <Button type="submit" className="ml-auto">
+                Entrar
+              </Button>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
     </div>
   );
 }
